8d3bc609d2d56518b82d65724e7f14645eb5ba37
Hast <hastyulia@users.noreply.github.com>
2019-10-30 17:28:33 +0500

    Изменила иконки погоды

---
+++ src/main/java/Icon.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Icons {
+    HashMap<String, String> icons = new HashMap<>();
+    Icons() {
+        icons.put("01d", "https://img.icons8.com/ios-filled/6x/sun.png");
+        icons.put("01n", "https://img.icons8.com/ios-filled/6x/moon.png");
+        icons.put("02d", "https://img.icons8.com/ios-filled/6x/partly-cloudy-day.png");
+        icons.put("02n", "https://img.icons8.com/ios-filled/6x/partly-cloudy-night.png");
+        icons.put("03d", "https://img.icons8.com/ios-filled/6x/clouds.png");
+        icons.put("03n", "https://img.icons8.com/ios-filled/6x/clouds.png");
+        icons.put("04d", "https://img.icons8.com/ios-filled/6x/skydrive.png");
+        icons.put("04n", "https://img.icons8.com/ios-filled/6x/skydrive.png");
+        icons.put("09d", "https://img.icons8.com/ios-filled/6x/moderate-rain.png");
+        icons.put("09n", "https://img.icons8.com/ios-filled/6x/moderate-rain.png");
+        icons.put("10d", "https://img.icons8.com/ios-filled/6x/partly-cloudy-rain.png");
+        icons.put("10n", "https://img.icons8.com/ios-filled/6x/light-rain-2.png");
+        icons.put("11d", "https://img.icons8.com/ios-filled/6x/cloud-lighting.png");
+        icons.put("11n", "https://img.icons8.com/ios-filled/6x/cloud-lighting.png");
+        icons.put("13d", "https://img.icons8.com/ios-filled/6x/snow.png");
+        icons.put("13n", "https://img.icons8.com/ios-filled/6x/snow.png");
+        icons.put("50d", "https://img.icons8.com/ios-filled/6x/fog-day.png");
+        icons.put("50n", "https://img.icons8.com/ios-filled/6x/fog-night.png");
+    }
+}
+
