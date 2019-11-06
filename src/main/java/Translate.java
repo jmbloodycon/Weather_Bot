a0c7a8908bc8fc341b2f435b154554334fedfaa0
jmbloodycon <mybloodycontract@yandex.ru>
2019-11-06 23:51:00 +0500

    add class Translate

---
+++ src/main/java/Translate.java
@@ -0,0 +1,33 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Translate {
+    private static int i = 0;
+
+    static String translate(String input) throws IOException {
+
+        String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + Files.readString(Paths.get("translateToken.txt"));
+        URL urlObj = new URL(urlStr);
+        HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+        dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, StandardCharsets.UTF_8) + "&lang=" + "en");
+
+        InputStream response = connection.getInputStream();
+        String json = new java.util.Scanner(response).nextLine();
+        int start = json.indexOf("[");
+        int end = json.indexOf("]");
+        String translated = json.substring(start + 2, end - 1);
+        i++;
+        return translated;
+    }
+}
